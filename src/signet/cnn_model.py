592376265f153584081fdbbcd3e8669b9af9083a
Arthur Flôr <arthurflor23@gmail.com>
2018-11-09 15:52:26 -0300

    clean code

--- src/signet/cnn_model.py
+++ src/signet/cnn_model.py
@@ -11,7 +11,6 @@ def extract_features(img_arr):
     return [model.get_feature_vector(img) for (i, img) in enumerate(img_arr)]
 
 class CNNModel:
-    """ Represents a model trained with the Lasagne library. """
 
     def __init__(self, model_factory, model_weight_path):
         """
@@ -37,7 +36,6 @@ class CNNModel:
             image (numpy.ndarray): The input image
             layer (str): The desired output layer
         """
-
         assert len(image.shape) == 2, "Input should have two dimensions: H x W"
         input = image[np.newaxis, np.newaxis]
 
@@ -48,25 +46,3 @@ class CNNModel:
 
         out = self.forward_util_layer[layer](input)
         return out
-
-    def get_feature_vector_multiple(self, images, layer='fc2'):
-        """ 
-        Runs forward propagation until a desired layer, for one input image
-        Parameters:
-            images (numpy.ndarray): The input images. 
-                Should have three dimensions: N x H x W, where N: number of images, H: height, W: width
-            layer (str): The desired output layer
-        """
-
-        images = np.asarray(images)
-        assert len(images.shape) == 3, "Input should have three dimensions: N x H x W"
-
-        input = np.expand_dims(images, axis=1)
-
-        if layer not in self.forward_util_layer:
-            inputs = T.tensor4('inputs')
-            outputs = lasagne.layers.get_output(self.model[layer], inputs=inputs, deterministic=True)
-            self.forward_util_layer[layer] = theano.function([inputs], outputs)
-
-        out = self.forward_util_layer[layer](input)
-        return out    
