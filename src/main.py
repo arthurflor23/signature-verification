46e5c14a7ee2281d4492c6b39628a7c308a02c3f
Arthur Flôr <arthurflor23@gmail.com>
2018-11-13 16:09:19 -0300

    Add results/report of the characters dataset experiment

--- src/main.py
+++ src/main.py
@@ -3,20 +3,20 @@ import util.data as data
 
 def main():
     
-    # dataset, repeat, save = "signatures", 30, True
-    data.DATASET, repeat, save = "characters", 30, True
+    data.DATASET, repeat, save = "signatures", 30, True
+    # data.DATASET, repeat, save = "characters", 30, True
 
     features, labels = data.loadDataset(data.DATASET, "mi_hu")
-
     decision_tree.random_forest(features, labels, repeat, save)
     decision_tree.cart(features, labels, repeat, save)
     decision_tree.c45(features, labels, repeat, save)
+    del features, labels
 
     features, labels = data.loadDataset(data.DATASET, "cnn")
-
     decision_tree.random_forest(features, labels, repeat, save)
     decision_tree.cart(features, labels, repeat, save)
     decision_tree.c45(features, labels, repeat, save)
+    del features, labels
 
 if __name__ == '__main__':
     main()
